efb430651ac36a4e87a3684da3ed8edd1dbf78e8
Kyle DeFrancia <kyleisntwild@gmail.com>
2013-10-23 23:47:52 -0700

    Add dropdown menu to select the search type.  Lookup button now causes the map to zoom and center itself in the correct place according to search type/ search term.  Other changes include minor code formatting changes (tabs to spaces, fix indentation) to some files and removal of some unnecessary code from Crenshaw that our project does not need.

--- public/js/components/admissions.js
+++ public/js/components/admissions.js
@@ -11,134 +11,146 @@
 
 define([], function() {
 
-	/*
-	 * regions
-	 *
-	 * An object that represents all of the region assignments in the
-	 * Admissions Unit.  Each property is a region name.  Each value
-	 * of each property is an array.  The first element of the array
-	 * is the two-letter state abbreviation associated with the region.
-	 * The remainder of the array is any subregions for the region.
-	 *
-	 * TODO: Incomplete list.  I don't feel, either, that this is the
-	 * best way to organize this data.
-	 */
-	var regions = {
-		"Alaska" : ["AK"],
-		"Arizona" : ["AZ"],
-		"Californa" : ["CA"],
-		"Colorado" : ["CO"],
-		"Hawaii" : ["HI"],
-		"Idaho" : ["ID"],
-		"Illinois" : ["IL"],
-		"Maine" : ["ME"],
-		"Minnesota" : ["MN"],
-		"Nebraska" : ["NE"],
-		"New Mexico" : ["NM"],
-		"New York" : ["NY"],
-		"North Nevada - Reno" : ["NV"],
-		"South Nevada - Las Vegas" : ["NV"],
-		"Oregon" : ["OR", "Salem", "Corvallis", "Eugene"],
-		"Portland" : ["OR", "Area1", "Catholic1", "Catholic2", "Area2", "Area3", "Area4", "North Coast"],
-		"Texas" : ["TX"],
-		"Utah" : ["UT"],
-		"Washington" : ["WA"]
+    /*
+     * regions
+     *
+     * An object that represents all of the region assignments in the
+     * Admissions Unit.  Each property is a region name.  Each value
+     * of each property is an array.  The first element of the array
+     * is the two-letter state abbreviation associated with the region.
+     * The remainder of the array is any subregions for the region.
+     *
+     * TODO: Incomplete list.  I don't feel, either, that this is the
+     * best way to organize this data.
+     */
+    var regions = {
+        "Alaska" : ["AK"],
+        "Arizona" : ["AZ"],
+        "Californa" : ["CA"],
+        "Colorado" : ["CO"],
+        "Hawaii" : ["HI"],
+        "Idaho" : ["ID"],
+        "Illinois" : ["IL"],
+        "Maine" : ["ME"],
+        "Minnesota" : ["MN"],
+        "Nebraska" : ["NE"],
+        "New Mexico" : ["NM"],
+        "New York" : ["NY"],
+        "North Nevada - Reno" : ["NV"],
+        "South Nevada - Las Vegas" : ["NV"],
+        "Oregon" : ["OR", "Salem", "Corvallis", "Eugene"],
+        "Portland" : ["OR", "Area1", "Catholic1", "Catholic2", "Area2", "Area3", "Area4", "North Coast"],
+        "Texas" : ["TX"],
+        "Utah" : ["UT"],
+        "Washington" : ["WA"]
 
-	};
+    };
 
-	/*
-	 * getRegions
-	 *
-	 * Based on the "Counselor Assignments" provided by Jason McDonald, this
-	 * method supplies an array of all of the Region Names for the Admissions
-	 * Unit, as group by counselor.
-	 *
-	 * TODO: TLC.  Currently, this is only a partial list, as I am doing a
-	 * proof of concept with regards to drop-down menus.  I need to finish
-	 * enumerating all the regions.
-	 *
-	 * @param none
-	 * @return regions An object containing each region as a property and
-	 * an array of all subregions as the value of each property.
-	 *
-	 */
-	var getRegions = function() {
+    /*
+     * getRegions
+     *
+     * Based on the "Counselor Assignments" provided by Jason McDonald, this
+     * method supplies an array of all of the Region Names for the Admissions
+     * Unit, as group by counselor.
+     *
+     * TODO: TLC.  Currently, this is only a partial list, as I am doing a
+     * proof of concept with regards to drop-down menus.  I need to finish
+     * enumerating all the regions.
+     *
+     * @param none
+     * @return regions An object containing each region as a property and
+     * an array of all subregions as the value of each property.
+     *
+     */
+    var getRegions = function() {
 
-		return regions;
-	};
-	/*
-	 * convertRegionToState
-	 *
-	 * Convert the possible menu choices available to a two-letter
-	 * state abbreviation.
-	 *
-	 * @param menuChoice A user-supplied menu choice extracted from an HTML
-	 * element.
-	 * @return state A two-letter state abbreviation.
-	 */
-	var convertRegionToState = function(region) {
+        return regions;
+    };
+    /*
+     * convertRegionToState
+     *
+     * Convert the possible menu choices available to a two-letter
+     * state abbreviation.
+     *
+     * @param menuChoice A user-supplied menu choice extracted from an HTML
+     * element.
+     * @return state A two-letter state abbreviation.
+     */
+    var convertRegionToState = function(region) {
 
-		var state = regions[region][0];
+        var state = regions[region][0];
 
-		return state;
+        return state;
 
-	};
-	/*
-	 * getHighSchoolName
-	 *
-	 * Based on the construction of the Fusion Table used to supply high
-	 * school address and CEEB information, this function extracts
-	 * the high school name from a Fusion Table response and returns
-	 * it as a string.
-	 *
-	 */
-	var getHighSchoolName = function(response) {
-		return response["rows"][0][0].toString();
-	};
-	/*
-	 * getHighSchoolAddress
-	 *
-	 * Based on the construction of the Fusion Table used to supply high
-	 * school address and CEEB information, this function extracts
-	 * the high school address from a Fusion Table response and returns
-	 * it as a string.
-	 *
-	 */
-	var getHighSchoolAddress = function(response) {
-		return response["rows"][0][1].toString();
-	};
-	/*
-	 * getZipcode
-	 *
-	 * Based on the construction of the Fusion Table used to supply high
-	 * school address and CEEB information, this function extracts
-	 * the high school zipcode from a Fusion Table response and returns
-	 * it as a string.
-	 *
-	 */
-	var getZipcode = function(response) {
-		return response["rows"][0][4].toString();
-	};
+    };
+    /*
+     * getHighSchoolName
+     *
+     * Based on the construction of the Fusion Table used to supply high
+     * school address and CEEB information, this function extracts
+     * the high school name from a Fusion Table response and returns
+     * it as a string.
+     *
+     */
+    var getHighSchoolName = function(response) {
+        return response["rows"][0][0].toString();
+    };
+    /*
+     * getHighSchoolAddress
+     *
+     * Based on the construction of the Fusion Table used to supply high
+     * school address and CEEB information, this function extracts
+     * the high school address from a Fusion Table response and returns
+     * it as a string.
+     *
+     */
+    var getHighSchoolAddress = function(response) {
+        return response["rows"][0][1].toString();
+    };
 
 
+    /*
+     * getZipcode
+     *
+     * Based on the construction of the Fusion Table used to supply high
+     * school address and CEEB information, this function extracts
+     * the high school zipcode from a Fusion Table response and returns
+     * it as a string.
+     *
+     */
+    var getZipcode = function(response) {
+        return response["rows"][0][4].toString();
+    };
 
-        /*
-         * getState
-         *
-         */
-        var getState = function(response) {
-                return response["rows"][0][3].toString();
-        };
 
-	// Any functions defined in this return statement are considered public
-	// functions by RequireJS, and accessible through the namespace that
-	// is attached to this module when loaded in other files.
-	return {
-		getRegions : getRegions,
-		convertRegionToState : convertRegionToState,
-		getHighSchoolName : getHighSchoolName,
-		getHighSchoolAddress : getHighSchoolAddress,
-		getZipcode : getZipcode,
-                getState : getState
-	};
+    /*
+     * getState
+     *
+     */
+    var getState = function(response) {
+        return response["rows"][0][3].toString();
+    };
+
+
+    /*
+     * getCity
+     *
+     */
+    var getCity = function(response) {
+        return response["rows"][0][2].toString();
+    };
+
+
+    // Any functions defined in this return statement are considered public
+    // functions by RequireJS, and accessible through the namespace that
+    // is attached to this module when loaded in other files.
+    return {
+        getRegions : getRegions,
+                   convertRegionToState : convertRegionToState,
+                   getHighSchoolName : getHighSchoolName,
+                   getHighSchoolAddress : getHighSchoolAddress,
+                   getZipcode : getZipcode,
+                   getState : getState,
+                   getCity : getCity
+    };
 });
--- public/js/components/usmap.js
+++ public/js/components/usmap.js
@@ -11,250 +11,280 @@
 
 define(['./utilities', './filterMenu', './admissions', './layers'], function(utilities, filterMenu, admissions, layers) {
 
-	/*
-	 *  lookup()
-	 *
-	 *  For an array of layers, this function filters the layers
-	 *  according to the CEEB grabbed from the page (that is,
-	 * 	entered by the user) and recenters the layer's map at the zipcode
-	 *  associated with the given CEEB.
-	 *
-	 *  This function assumes that all the layers are on the same map.
-	 *  Thus, when resetting the zoom and recentering the map, these
-	 *  operations are done only once on the first layer.
-	 *
-	 *  @param layerArray An array of Google FusionTableLayers.
-	 *  @param geocoder The geocoder service to use to recenter the map.
-	 *  @return void
-	 */
+    /*
+     *  lookup()
+     *
+     *  For an array of layers, this function filters the layers
+     *  according to the CEEB grabbed from the page (that is,
+     *     entered by the user) and recenters the layer's map at the zipcode
+     *  associated with the given CEEB.
+     *
+     *  This function assumes that all the layers are on the same map.
+     *  Thus, when resetting the zoom and recentering the map, these
+     *  operations are done only once on the first layer.
+     *
+     *  @param layerArray An array of Google FusionTableLayers.
+     *  @param geocoder The geocoder service to use to recenter the map.
+     *  @return void
+     */
 
-	var lookup = function(layerArray, geocoder) {
+    var lookup = function(layerArray, geocoder) {
 
-		// Get the CEEB code from the page, as entered by the user.
-		var ceeb = utilities.getCEEB();
+        // Get search type
+        var stype = utilities.getSearchType();
 
-		// Did the user type anything?
-		if(ceeb == "") {
+        // Get the search term from the page, as entered by the user.
+        var sterm = utilities.getSearchTerm();
 
-			// Give an error message to the user:
-			utilities.getErrorMsgElement().innerHTML = "Error: Please enter a CEEB";
-		}
+        // Did the user type anything?
+        if(sterm == "") {
 
-		// Did the user enter the CEEB?
-		else {
-			// Clear any error message.
-			utilities.getErrorMsgElement().innerHTML = "";
+            // Give an error message to the user:
+            utilities.getErrorMsgElement().innerHTML = "Error: Please enter something to search for";
 
-			// Get the corresponding zipcode for this CEEB.  The zipcode for this
-			// CEEB is contained in the same row as the CEEB in the Google Fusion
-			// tables.  So, I need to get the Zip in the row whose 'Code' is
-			// the CEEB entered by the user.  This is done using the GET API
-			// specified in the Google Fusion Tables Developer documentation.
+        } else {
 
-			// Create a url for
-			// a subsequent GET request to a Google server.
-			var query = "SELECT * FROM " + layerArray[0].eID + " WHERE Code = " + ceeb;
+            // Clear any error message.
+            utilities.getErrorMsgElement().innerHTML = "";
 
-			var url = "https://www.googleapis.com/fusiontables/v1/query";
-			url = url + "?sql=" + query;
-			url = url + " &key=" + apikey;
+            // Get the corresponding zipcode for this CEEB.  The zipcode for this
+            // CEEB is contained in the same row as the CEEB in the Google Fusion
+            // tables.  So, I need to get the Zip in the row whose 'Code' is
+            // the CEEB entered by the user.  This is done using the GET API
+            // specified in the Google Fusion Tables Developer documentation.
 
-			function handleResponse() {
-				if(httpRequest.readyState === 4) {
-					if(httpRequest.status === 200) {
+            // Actually, just get all the information for a given search term.
 
-						// The code reaches this point because the Google server
-						// responded with some useful data.
+            // Create a url for
+            // a subsequent GET request to a Google server.
+            var query = "SELECT * FROM " + layerArray[0].eID + " WHERE Code = " + sterm;
 
-						// The response is just a string.  I need
-						// to parse it so that I can extract the zip code from it.
-						var response = JSON.parse(httpRequest.responseText);
+            var url = "https://www.googleapis.com/fusiontables/v1/query";
+            url = url + "?sql=" + query;
+            url = url + " &key=" + apikey;
 
-						if(response["rows"] != undefined) {
-							// Set 	the zoom.
-							layerArray[0].map.setZoom(6);
+            function handleResponse() {
+                if(httpRequest.readyState === 4) {
+                    if(httpRequest.status === 200) {
 
-							var highSchoolName = admissions.getHighSchoolName(response);
-							var highSchoolAddress = admissions.getHighSchoolAddress(response);
-							var zipcode = admissions.getZipcode(response);
-                                                        var state = admissions.getState(response);
+                        // The code reaches this point because the Google server
+                        // responded with some useful data.
 
-							// Center the map.
-							centerAt(layerArray[0].map, state, geocoder);
+                        var response = JSON.parse(httpRequest.responseText);
 
-							// Filter the zip layer by zip
-							//layers.filterByZip.call(layerArray[0], zipcode);
+                        if(response["rows"] != undefined) {
+                            // Set the zoom.
+                            var zoomLvl = getZoomLevel(stype);
+                            layerArray[0].map.setZoom(zoomLvl);
 
-							// Filter the school layer by CEEB.
-							layers.filterByCEEB.call(layerArray[0], ceeb);
+                            var hsName = admissions.getHighSchoolName(response);
+                            var hsAddress = admissions.getHighSchoolAddress(response);
+                            var zipcode = admissions.getZipcode(response);
+                            var city = admissions.getCity(response);
+                            var state = admissions.getState(response);
 
-							// Display the textual result
-							utilities.getRightSidebarElement().innerHTML = highSchoolName + '<br>' + highSchoolAddress;
+                            // Center the map.
+                            var center;
+                            switch(stype) {
+                                case "ceeb":
+                                case "hsname":
+                                    center = hsAddress;
+                                    break; 
+                                case "zip":
+                                    center = zipcode;
+                                    break;
+                                case "state":
+                                    center = state;
+                                    break;
+                                case "city":
+                                    center = city + " " + state;
+                                    break;
+                            }
+                            centerAt(layerArray[0].map, center, geocoder);
 
-						} else {
-							// Indicate to the user that I could not find that
-							// CEEB.
-							utilities.getErrorMsgElement().innerHTML = "Cannot locate CEEB: " + ceeb + ".";
-						}
+                            // Filter the school layer by CEEB.
+                            layers.filterByCEEB.call(layerArray[0], sterm);
 
-					}
-				}
-			}
+                            // Display the textual result
+                            utilities.getRightSidebarElement().innerHTML = hsName + '<br>' + hsAddress;
 
-		}
+                        } else {
+                            // Indicate to the user their search term was not found 
+                            utilities.getErrorMsgElement().innerHTML = "Cannot locate " + stype + ": " + sterm + ".";
+                        }
 
-		// Send the GET Request to the Google Server
-		utilities.sendRequest(url, handleResponse);
-	};
-	/*
-	 *  centerAt()
-	 *
-	 *	Given an address, use the Google geocode service to obtain the
-	 *  latitude and longitude points for that address and center the
-	 *  window object's 'map' at the lat/lon point provided.
-	 *
-	 *  Note that the Google geocoder service is an asynchronous call,
-	 *  so an anonymous callback function is used to handle the result.
-	 *  Otherwise, the page will hang.
-	 *
-	 *  Based largely on the geocode sample provided in the Google Developer
-	 *  documentation for Google Maps API 3 for Javascript.
-	 *
-	 *  @param address A string representing an address, such as "Chicago"
-	 *  or "92171"
-	 *
-	 *  @return void
-	 * */
-	var centerAt = function(map, address, geocoder) {
+                    }
+                }
+            }
 
-		geocoder.geocode({
-			'address' : address
-		}, function(results, status) {
-			if(status == google.maps.GeocoderStatus.OK) {
-				map.setCenter(results[0].geometry.location);
-			} else {
-				alert("Geocode was not successful for the following reason: " + status);
-			}
-		});
-	};
-	/*
-	 *  regionalize()
-	 *
-	 *  This function filters an array of layer objects based on the
-	 *  region name grabbed from the page (that is, entered by the user)
-	 *  and recenters the map at the region.
-	 *
-	 *  @param layerArray An array of Layer objects to regionalize.
-	 *  @param geocoder The geocoder service to use to recenter the map.
-	 *  @return void
-	 */
-	var regionalize = function(layerArray, geocoder) {
+        }
 
-		// Get the region name from the page, as entered by the user.
-		var region = utilities.getRegion();
+        // Send the GET Request to the Google Server
+        utilities.sendRequest(url, handleResponse);
+    };
 
-		// Filter each layer by the region.
-		utilities.forEach(layerArray, function(t) {
-			layers.filterByRegion.call(t, region);
-		});
-		// Set the zoom.
-		layerArray[0].map.setZoom(6);
 
-		// Center the map at the state corresponding to the region.
-		centerAt(layerArray[0].map, admissions.convertRegionToState(region), geocoder);
-	};
-	/*
-	 *  initialize()
-	 *
-	 *  A function to:
-	 *  1. create a google map centered at the us
-	 *  2. Initialize two layer objects, one for zipcodes and one for schools.
-	 *  3. instantiate a google maps geocoder service.
-	 *
-	 *  @param void
-	 *  @return void
-	 */
-	var initialize = function() {
+    /*
+     * getZoomLevel()
+     * 
+     * Given a search type, return the appropriate zoom level to 
+     * best display the data on the map.
+     *
+     * @param stype A search type, such as "ceeb" or "state"
+     * @return An integer zoom level
+     *
+     */
+    var getZoomLevel = function(stype) {
 
-		// Set the Google API key for this namespace.
-		apikey = 'AIzaSyDPIDblZh-H55gEkg7u6KVry1OZ-pgGpsQ';
+        var zoom;
 
-		// Create the httpRequestor for this namespace.
-		utilities.makeRequestor();
+        switch(stype) {
+            case "zip":
+            case "ceeb":
+            case "hsname":
+                zoom = 12;
+                break;
+            case "state":
+                zoom = 6;
+                break;
+            case "city":
+                zoom = 11;
+                break;
+            default:
+                zoom = 7;
+        }
 
-		// Encrypted IDs for the Google Fusion Table containing the
-		// Oregon high school and CEEB data.
-		var schoolEID = '1TysRKf1siV396AMbUKmi8w2-XB3Zeye2ObXjl8Y';
+        return zoom;
+    }
 
-		// The Encrypted ID used below is that of tl_2010_41_zcta051_clean.kml
-		// available in Tanya Crenshaw's public fusion tables.
-		var zipEID = '1U6n-9O6I9q4qG10uNcNrCAlvactfL7O07IVPLbU';
 
-		// Instatiate a new geocoder service
-		var geocoder = new google.maps.Geocoder();
+    /*
+     *  centerAt()
+     *
+     *    Given an address, use the Google geocode service to obtain the
+     *  latitude and longitude points for that address and center the
+     *  window object's 'map' at the lat/lon point provided.
+     *
+     *  Note that the Google geocoder service is an asynchronous call,
+     *  so an anonymous callback function is used to handle the result.
+     *  Otherwise, the page will hang.
+     *
+     *  Based largely on the geocode sample provided in the Google Developer
+     *  documentation for Google Maps API 3 for Javascript.
+     *
+     *  @param address A string representing an address, such as "Chicago"
+     *  or "92171"
+     *
+     *  @return void
+     * */
+    var centerAt = function(map, address, geocoder) {
 
-		// The center point of the map is Lincoln, NB.
-		var usa = new google.maps.LatLng(40.8358, -96.6452);
+        geocoder.geocode({
+            'address' : address
+        }, function(results, status) {
+            if(status == google.maps.GeocoderStatus.OK) {
+                map.setCenter(results[0].geometry.location);
+            } else {
+                alert("Geocode was not successful for the following reason: " + status);
+            }
+        });
+    };
 
-		// Create a Google Map object centered at usa
-		var map = new google.maps.Map(document.getElementById("contig_us_map_canvas"), {
-			zoom : 4,
-			center : usa,
-			mapTypeId : google.maps.MapTypeId.ROADMAP
-		});
-		//
-		//	Construct the layers
-		//
 
-		// Create a Layer object for the zip code boundary layer.  The second parameter
-		// creates the Google FusionTablesLayer object.  The Layer is not currently being
-		// filtered, so the final parameter is false.
-		// var zipLayer = new layers.Layer("zips", new google.maps.FusionTablesLayer({
-		// 	query : {
-		// 		from : zipEID
-		// 	}
-		// }), zipEID, map, 'ZipCodeArea', false);
+    /*
+     *  regionalize()
+     *
+     *  This function filters an array of layer objects based on the
+     *  region name grabbed from the page (that is, entered by the user)
+     *  and recenters the map at the region.
+     *
+     *  @param layerArray An array of Layer objects to regionalize.
+     *  @param geocoder The geocoder service to use to recenter the map.
+     *  @return void
+     */
+    var regionalize = function(layerArray, geocoder) {
 
-		// Create a Layer object for the high schools layer.  The second parameter
-		// creates the Google FusionTablesLayer object.  The Layer is not currently being
-		// filtered, so the final parameter is false.
-		var schoolLayer = new layers.Layer("schools", new google.maps.FusionTablesLayer({
-			query : {
-				from : schoolEID
-			}
-		}), schoolEID, map, 'Zip', false);
+        // Get the region name from the page, as entered by the user.
+        var region = utilities.getRegion();
 
-		// Create an array of Layers
-		var layerArray = new Array();
-		// layerArray.push(zipLayer);
-		layerArray.push(schoolLayer);
+        // Filter each layer by the region.
+        utilities.forEach(layerArray, function(t) {
+            layers.filterByRegion.call(t, region);
+        });
+        // Set the zoom.
+        layerArray[0].map.setZoom(6);
 
-		// Attach the function lookup() to the lookupButton on the main page.
-		utilities.addEvent(document.getElementById('lookupButton'), 'click', function() {
-			return lookup(layerArray, geocoder);
-		});
-		//
-		// Constructing the Filter Menus
-		//
-		// Create the Region Filtering menu
-		// Connect the filter menu to the "regionFilterPanel" that is on the index.html page.
-		// Get the menu options from the admissions domain model method, getRegions().
-		// Attach the method filter() to the menu such that whenever the menu changes,
-		// the filter() method is called.
-		var regionFilterMenu = new filterMenu.FilterMenu("regionFilterMenu", "regionFilterMenu", "filterMenu", "regionFilterPanel", admissions.getRegions(), function() {
-			return regionalize(layerArray, geocoder);
-		});
-		regionFilterMenu.createMenu();
+        // Center the map at the state corresponding to the region.
+        centerAt(layerArray[0].map, admissions.convertRegionToState(region), geocoder);
+    };
 
-	};
-	// Any functions defined in this return statement are considered public
-	// functions by RequireJS, and accessible through the namespace that
-	// is attached to this module when loaded in other files.
-	return {
 
-		lookup : lookup,
-		centerAt : centerAt,
-		regionalize : regionalize,
-		initialize : initialize
-	};
+    /*
+     *  initialize()
+     *
+     *  A function to:
+     *  1. create a google map centered at the us
+     *  2. Initialize one layer object, for high schools.
+     *  3. instantiate a google maps geocoder service.
+     *
+     *  @param void
+     *  @return void
+     */
+    var initialize = function() {
+
+        // Set the Google API key for this namespace.
+        apikey = 'AIzaSyDPIDblZh-H55gEkg7u6KVry1OZ-pgGpsQ';
+
+        // Create the httpRequestor for this namespace.
+        utilities.makeRequestor();
+
+        // Encrypted IDs for the Google Fusion Table containing the
+        // Oregon high school and CEEB data.
+        var schoolEID = '1TysRKf1siV396AMbUKmi8w2-XB3Zeye2ObXjl8Y';
+
+        // Instatiate a new geocoder service
+        var geocoder = new google.maps.Geocoder();
+
+        // The center point of the map is Lincoln, NB.
+        var usa = new google.maps.LatLng(40.8358, -96.6452);
+
+        // Create a Google Map object centered at usa
+        var map = new google.maps.Map(document.getElementById("contig_us_map_canvas"), {
+            zoom : 4,
+            center : usa,
+            mapTypeId : google.maps.MapTypeId.ROADMAP
+        });
+        //
+        //    Construct the layers
+        //
+
+        // Create a Layer object for the high schools layer.  The second parameter
+        // creates the Google FusionTablesLayer object.  The Layer is not currently being
+        // filtered, so the final parameter is false.
+        var schoolLayer = new layers.Layer("schools", new google.maps.FusionTablesLayer({
+            query : {
+                from : schoolEID
+            }
+        }), schoolEID, map, 'Zip', false);
+
+        // Create an array of Layers
+        var layerArray = new Array();
+        layerArray.push(schoolLayer);
+
+        // Attach the function lookup() to the lookupButton on the main page.
+        utilities.addEvent(document.getElementById('lookupButton'), 'click', function() {
+            return lookup(layerArray, geocoder);
+        });
+    };
+    // Any functions defined in this return statement are considered public
+    // functions by RequireJS, and accessible through the namespace that
+    // is attached to this module when loaded in other files.
+    return {
+
+        lookup : lookup,
+        getZoomLevel : getZoomLevel,
+        centerAt : centerAt,
+        regionalize : regionalize,
+        initialize : initialize
+    };
 });
